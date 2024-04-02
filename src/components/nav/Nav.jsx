61d3b006ad8d1920a3ea40bcc384d693c433ff96
tiwarimanvi <manvitiwari029@gmail.com>
2024-04-03 00:07:38 +0530

    starting with navbar

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -3,6 +3,7 @@ import { GoHome } from "react-icons/go";
 import { LuUser2 } from "react-icons/lu";
 import { RiBook2Line } from "react-icons/ri";
 import { RiServiceLine } from "react-icons/ri";
+import { BiMessageSquareDetail } from "react-icons/bi";
 
 
 import './nav.css'
@@ -13,7 +14,7 @@ const Nav = () => {
       <a href="#about"><LuUser2 /></a>
       <a href="#experience"><RiBook2Line /></a>
       <a href="#services"><RiServiceLine /></a>
-      <a href="#contact"><GoHome/></a>
+      <a href="#contact"><BiMessageSquareDetail /></a>
     </nav>
   )
 }
